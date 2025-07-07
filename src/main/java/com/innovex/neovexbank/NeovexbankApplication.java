5b125fb6eddbb30e6c8b0494e1c5ac3216810ccb
ferruzxca <ferruzxca@gmail.com>
2025-07-06 22:28:44 -0600

    Commit

---
+++ src/main/java/com/innovex/neovexbank/NeovexbankApplication.java
@@ -0,0 +1,13 @@
+package com.innovex.neovexbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeovexbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeovexbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovex/neovexbank/NeovexbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovex.neovexbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeovexbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
